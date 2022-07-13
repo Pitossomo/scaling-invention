d508f7710b708addf2a5e5979f034ba8553cb4f4
pitossomo <pedro.h.teles@hotmail.com>
2022-07-13 05:34:29 -0300

    style improved

--- script.js
+++ script.js
@@ -8,9 +8,20 @@ function handleSubmit(event) {
   event.preventDefault()
   let cli = document.getElementById("command-line")
   const command = cli.value
- 
-  const newLogLine = document.createElement("p")
-  newLogLine.appendChild(document.createTextNode(command))
-  document.getElementById("log").appendChild(newLogLine)
+
+  let newLogLine = document.createElement("div")
+  newLogLine.classList.add("log-line")
+
+  let preLine = document.createElement("p")
+  preLine.classList.add("preline")
+  preLine.appendChild(document.createTextNode(">>"))
+  newLogLine.appendChild(preLine)
+
+  let log = document.createElement("p")
+  log.classList.add("log")
+  log.appendChild(document.createTextNode(command))
+  newLogLine.appendChild(log)
+
+  document.getElementById("log-history").appendChild(newLogLine)
   cli.value = ""
 }
